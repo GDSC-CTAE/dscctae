df6dcadf451d6f9caea37bb4d7d84e489288f0d6
Sheetal575 <sheetaldadh9@gmail.com>
2021-10-27 22:33:55 +0530

    see all project button added

--- dscctae/src/components/Projects/Projects.js
+++ dscctae/src/components/Projects/Projects.js
@@ -5,10 +5,11 @@ import { projectData } from "../../data/projectsData";
 
 function Project() {
   return (
-    <div className="container mt-5" id="projects">
+    <div className="container mt-3" id="projects">
       <div>
         <h1>Projects</h1>
       </div>
+      <br />
       <div className="row mt-5 justify-content-around">
         {projectData.map((projects, index) => {
           return (
@@ -17,7 +18,7 @@ function Project() {
               key={index}
             >
               <h3>{projects.title}</h3>
-              <div className="mt-3">{projects.description}</div>
+              <div className="mt-3 project-description">{projects.description}</div>
               <div>
                 <hr />
               </div>
@@ -32,7 +33,7 @@ function Project() {
                   </span>
                 </div>
                 <div className="col-3 button-container">
-                  <button className=" github-button  ">
+                  <button className=" github-button  " onClick={()=>{window.open("https://github.com","_blank")}}>
                     <GitHubIcon className="fs-1 h-100 w-100" />
                   </button>
                 </div>
@@ -46,6 +47,12 @@ function Project() {
             </div>
           );
         })}
+      </div>
+      <br />
+      <div>
+          <button className="contribute-button rounded p-3 mt-5 ">
+                  See All projects
+          </button>
       </div>
     </div>
   );
