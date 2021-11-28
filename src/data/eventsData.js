02fe86f3253b71152869e892ff300eedeb2a3550
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2021-11-28 11:26:50 +0530

    Changes

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -22,24 +22,4 @@ export const eventsData = [
     link: "https://codeforces.com/gym/351260/standings",
     img: eventImg3,
   },
-  {
-    eventName: "Open Talk with DSC CTAE Team",
-    description:
-      "A general meeting for all the team members where they can ask anything about the club and their leads of respective domain. You can ask your doubts regarding tech, community and leadership. ",
-    link: "https://gdsc.community.dev/events/details/developer-student-clubs-college-of-technology-and-engineering-ctae-udaipur-presents-open-talk-with-dsc-ctae-team/",
-    img: eventImg,
-  },
-  {
-    eventName: "Talk on Open Source",
-    description:
-      "Talk on Open Source along with info session on Month-Long Open Source Event in which there will be multiple sessions and workshops to guide you through your Open Source Journey.",
-    link: "https://gdsc.community.dev/events/details/developer-student-clubs-college-of-technology-and-engineering-ctae-udaipur-presents-talk-on-open-source/",
-    img: eventImg2,
-  },
-  {
-    eventName: "CodeCake 1.0",
-    description: "2 Hour long competitive coding contest on codeforces.com",
-    link: "https://codeforces.com/gym/351260/standings",
-    img: eventImg3,
-  },
 ];
